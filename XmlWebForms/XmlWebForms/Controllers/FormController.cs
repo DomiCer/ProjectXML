a2ddb62cb1e9dc174b3ffadd44e3d6bf3462fbf4
Robert Kocian <robert.kocian@gmail.com>
2015-03-03 11:43:35 +0100

    inicializacny commint
    pridany sln pre mvc, a vygenerovany form bez nakladov

---
+++ XmlWebForms/XmlWebForms/Controllers/FormController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XmlWebForms.Controllers
+{
+    public class FormController : Controller
+    {
+        //
+        // GET: /Form/
+
+        public ActionResult index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
