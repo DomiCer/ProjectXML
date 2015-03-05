df2cbb811184ba87c807f1ed6a0b7e8d9ee83e54
Robert Kocian <robert.kocian@gmail.com>
2015-03-05 01:41:13 +0100

    validacia XML voci CSD

--- XmlWebForms/XmlWebForms/Controllers/FormController.cs
+++ XmlWebForms/XmlWebForms/Controllers/FormController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Xml.Schema;
 using XmlWebForms.Models;
 namespace XmlWebForms.Controllers
 {
@@ -16,7 +21,48 @@ namespace XmlWebForms.Controllers
             return View(new dotacia());
         }
 
+        [HttpPost]
+        
+        public ActionResult index(Models.dotacia dotaciaObj)
+        {
+            if (Request.Form["ValidateXML"] !=null )
+            {
+                XmlSchemaSet schemas = new XmlSchemaSet();
+                schemas.Add("", @"C:\D\Skola\STUBA\2roc\Letny\SISS\Project\ProjectXML\Resources\dotacie.xsd");
 
+                Console.WriteLine("Attempting to validate");
+                XDocument custOrdDoc = CreateXML(dotaciaObj);  
+                bool errors = false;
+                custOrdDoc.Validate(schemas, (o, e) =>
+                {
+                    Console.WriteLine("{0}", e.Message);
+                    errors = true;
+                });
+                string result = errors ? "did not validate" : "validated";
+                ViewBag.errors = errors;
+                ViewBag.xml = custOrdDoc.ToString();
+            }
+            else  if (Request.Form["ValidateXSL"] !=null ){
+            
+            }
+            return View(dotaciaObj);
+        }
 
+       
+        public XDocument CreateXML(Object ClassObject)
+        {
+            XDocument xmlDoc = new XDocument();   //Represents an XML document, 
+            // Initializes a new instance of the XmlDocument class.          
+            XmlSerializer xmlSerializer = new XmlSerializer(ClassObject.GetType());
+            // Creates a stream whose backing store is memory. 
+            using (MemoryStream xmlStream = new MemoryStream())
+            {
+                xmlSerializer.Serialize(xmlStream, ClassObject);
+                xmlStream.Position = 0;
+                //Loads the XML document from the specified string.
+                xmlDoc = XDocument.Load(xmlStream);
+                return xmlDoc;
+            }
+        }
     }
 }
