abe84b3218681b81f48625a29e90a2f06bfc83a6
DomiCer <cervenova.dominika@gmail.com>
2015-05-20 08:39:18 +0200

    pridany overovac

---
+++ XmlWebForms/Overovac/Overovac.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Overovac
+{
+    partial class Overovac
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.btnOverit = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtLog);
+            this.panel1.Controls.Add(this.btnOverit);
+            this.panel1.Controls.Add(this.btnBrowse);
+            this.panel1.Controls.Add(this.txtFile);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(520, 322);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtLog.Location = new System.Drawing.Point(0, 143);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(520, 179);
+            this.txtLog.TabIndex = 4;
+            this.txtLog.Text = "";
+            // 
+            // btnOverit
+            // 
+            this.btnOverit.Location = new System.Drawing.Point(224, 86);
+            this.btnOverit.Name = "btnOverit";
+            this.btnOverit.Size = new System.Drawing.Size(75, 23);
+            this.btnOverit.TabIndex = 3;
+            this.btnOverit.Text = "Overiť";
+            this.btnOverit.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(407, 31);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(32, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(110, 33);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(291, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nahrať súbor";
+            // 
+            // Overovac
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 322);
+            this.Controls.Add(this.panel1);
+            this.Name = "Overovac";
+            this.Text = "Overovač elektronického podpisu";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Button btnOverit;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ XmlWebForms/Overovac/Overovac.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Overovac
+{
+    public partial class Overovac : Form
+    {
+
+        public XmlDocument XmlDoc { get; set; }
+
+        public Overovac()
+        {
+            InitializeComponent();
+            XmlDoc = new XmlDocument();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            ofd.Multiselect = false;
+            ofd.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            ofd.Filter = "súbory XML|*.xml";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFile.Text = ofd.FileName;
+                XmlDoc.Load(ofd.FileName);
+            }
+        }
+
+       
+    }
+}
