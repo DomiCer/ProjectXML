f3355f39f6002727854f2f4e2f580f6690a19f6e
Robert Kocian <robert.kocian@gmail.com>
2015-05-21 00:22:21 +0200

    uprava XmlSignature

--- XmlWebForms/Overovac/Verification/XmlSignature.cs
+++ XmlWebForms/Overovac/Verification/XmlSignature.cs
@@ -13,52 +13,71 @@ namespace Overovac.Verification
         public XmlSignature(XmlDocument xmlDoc) {
             XmlDoc = xmlDoc;
             NoError = true;
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(XmlDoc.NameTable);
+            nsmgr.AddNamespace("ds", "http://www.w3.org/2000/09/xmldsig#");
+            signedInfo = null;
+            signatureValue = null;
+            signatureNode = null;
         }
 
         public bool Validate() {
             ExistSignatureAtributes();
+            ExistSignedInfoAtributes();
+            CheckSignedValueID();
+
             return NoError;
         }
 
         private XmlDocument XmlDoc { get; set; }
         private bool NoError { get; set; }
-
-      
+        private XmlNamespaceManager Nsmgr { get; set; }
+        XmlNode signedInfo {get; set;}
+        XmlNode signatureValue { get; set; }
+        XmlNode signatureNode { get; set; }
 
         private void ExistSignatureAtributes()
         {
             bool noError = true;
-            XmlNode signedInfo = null;
-            var signatureNode = XmlDoc.GetElementsByTagName("ds:Signature").Count == 0 ? null : XmlDoc.GetElementsByTagName("ds:Signature").Item(0);
+           
+            signatureNode = XmlDoc.GetElementsByTagName("ds:Signature").Count == 0 ? null : XmlDoc.GetElementsByTagName("ds:Signature").Item(0);
             if (signatureNode == null)
-                noError = false;
+                NoError = false;
             else
             {
                 if (signatureNode.Attributes.GetNamedItem("Id") == null && string.IsNullOrEmpty(signatureNode.Attributes.GetNamedItem("Id").Value))
-                    noError = false;
-                XmlNamespaceManager nsmgr = new XmlNamespaceManager(XmlDoc.NameTable);
-                nsmgr.AddNamespace("ds", "http://www.w3.org/2000/09/xmldsig#");
-                if (signatureNode.Attributes.GetNamedItem("xmlns:ds") == null && string.IsNullOrEmpty(signatureNode.Attributes.GetNamedItem("xmlns:ds").Value))
-                    noError = false;
-
-                if (signatureNode.SelectSingleNode("//ds:Signature//ds:SignedInfo", nsmgr) == null)
-                    noError = false;
-                else
-                    signedInfo = signatureNode.SelectSingleNode("//ds:Signature//ds:SignedInfo", nsmgr);
-                if (signedInfo == null)
-                    noError = false;
-               // else
-               //     CheckAllSignedInfoAtributes(signedInfo);
+                    NoError = false;
                
+                if (signatureNode.Attributes.GetNamedItem("xmlns:ds") == null && string.IsNullOrEmpty(signatureNode.Attributes.GetNamedItem("xmlns:ds").Value))
+                    NoError = false;
+   
             }
 
-            if (!noError)
-                NoError = noError;
         }
 
-        private bool CheckAllSignedInfoAtributes(XmlNode signedInfo) {
+        private void ExistSignedInfoAtributes()
+        {
+            bool noError = true;
+             if (signatureNode.SelectSingleNode("//ds:Signature//ds:SignedInfo", Nsmgr) == null)
+                    noError = false;
+            else
+              signedInfo = signatureNode.SelectSingleNode("//ds:Signature//ds:SignedInfo", Nsmgr);
+              if (signedInfo == null)
+                  noError = false;
+              if (!noError)
+                  NoError = noError;
+        }
 
-            throw new NotImplementedException();
+        private void CheckSignedValueID() {
+            // signatureValue
+            if (signatureNode.SelectSingleNode("//ds:Signature//ds:SignatureValue", Nsmgr) == null)
+                NoError = false;
+            else
+            {
+                signatureValue = signatureNode.SelectSingleNode("//ds:Signature//ds:SignatureValue", Nsmgr);
+                if (signatureValue.Attributes.GetNamedItem("Id") == null && string.IsNullOrEmpty(signatureValue.Attributes.GetNamedItem("Id").Value))
+                    NoError = false;
+            }
+            
         }
     }
 }
